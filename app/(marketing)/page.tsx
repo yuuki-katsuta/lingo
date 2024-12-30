9209d52e6c91485a9054d189f75efcd496546bff
tpryuki <reptpr0314@gmail.com>
2024-12-31 00:42:48 +0900

    🚧

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,3 +1,42 @@
+import { Button } from '@/components/ui/button';
+import { ClerkLoaded, ClerkLoading, SignInButton, SignUpButton, SignedIn, SignedOut } from '@clerk/nextjs';
+import { Loader } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>This is Marketing Page</div>;
+  return (
+    <div className="mx-auto flex w-full max-w-[988px] flex-1 flex-col items-center justify-center gap-2 p-4 lg:flex-row">
+      <div className="relative mb-8 h-[240px] w-[240px] lg:mb-0 lg:h-[424px] lg:w-[424px]">
+        <Image src="/hero.svg" alt="hero" fill />
+      </div>
+      <div className="flex max-w-[480px] flex-col items-center gap-y-8 text-center text-neutral-600">
+        <h1 className="text-xl font-bold lg:text-3xl">Learn, practice, and master new languages with Lingo. </h1>
+        <div className="flex w-full max-w-[330px] flex-col items-center gap-y-3">
+          <ClerkLoading>
+            <Loader className="h-5 w-5 animate-spin text-muted-foreground" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton mode="modal" fallbackRedirectUrl="/learn">
+                <Button size="lg" variant="secondary" className="w-full">
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignInButton mode="modal" fallbackRedirectUrl="/learn">
+                <Button size="lg" variant="primaryOutline" className="w-full">
+                  I already have an account
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button size="lg" variant="secondary" className="w-full" asChild>
+                <Link href="/learn">Continue Learning</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
+    </div>
+  );
 }
