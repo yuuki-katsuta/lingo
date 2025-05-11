6caf510c8349b650c93909db5386e23639cf82f1
tpryuki <reptpr0314@gmail.com>
2025-05-11 22:30:38 +0900

    🚧

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -3,9 +3,10 @@ import Image from 'next/image';
 import { InfinityIcon } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
+import { courses } from '@/db/schema';
 
 type Props = {
-  activeCourse: any;
+  activeCourse: typeof courses.$inferSelect;
   hearts: number;
   points: number;
   hasActiveSubscription: boolean;
